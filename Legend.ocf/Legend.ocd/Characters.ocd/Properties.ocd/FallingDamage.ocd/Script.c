ebf7b924ce7455af06fb1cc1a6316eedbf728cc3
Mark <markh4b@hotmail.com>
2017-06-09 22:43:31 +0200

    Falling damage module

---
+++ Legend.ocf/Legend.ocd/Characters.ocd/Properties.ocd/FallingDamage.ocd/Script.c
@@ -0,0 +1,51 @@
+
+func Hit(int speed_x, int speed_y)
+{
+	CreateEffect(FxFallingDamage, 1, 5, speed_x / 10, speed_y / 10);
+	_inherited(speed_x, speed_y, ...);
+}
+
+
+func DoRoll()
+{
+	// rolling reduces falling damage
+	var falling_damage = GetEffect("FxFallingDamage", this);
+	
+	if (falling_damage)
+	{
+		falling_damage.remaining_damage -= falling_damage.remaining_damage / 2;
+	}
+
+	_inherited(...);
+}
+
+
+local FxFallingDamage = new Effect
+{
+	Construction = func (int speed_x, int speed_y)
+	{
+		var velocity = Distance(speed_x, speed_y);
+		this.total_damage = Max(0, velocity / 4 - 10);
+
+		if (this.total_damage > 0)
+		{
+			this.remaining_damage = this.total_damage;
+			this.Target->~CatchBlow();
+		}
+	},
+	
+	Timer = func ()
+	{
+		if (this.remaining_damage > 0)
+		{
+			var damage = Min(this.remaining_damage, 2);
+			this.remaining_damage -= damage;			
+			this.Target->DoEnergy(-damage);
+			return FX_OK;
+		}
+		else
+		{
+			return FX_Execute_Kill;
+		}
+	},
+};
