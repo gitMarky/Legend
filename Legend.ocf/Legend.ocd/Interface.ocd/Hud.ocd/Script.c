9f680a81c52145579f6f399322935c615f81e81f
Mark <markh4b@hotmail.com>
2017-06-09 06:39:09 +0200

    GUI: Action bar

--- Legend.ocf/Legend.ocd/Interface.ocd/Hud.ocd/Script.c
+++ Legend.ocf/Legend.ocd/Interface.ocd/Hud.ocd/Script.c
@@ -2,6 +2,7 @@
 // Include the different subsystems of the HUD. They all handle their part
 // themselves via overloading of callbacks.
 #include GUI_Controller_InventoryBar
+#include GUI_Controller_ActionBar
 #include GUI_Component_Health
 // Include the basic functionality of the HUD
 #include Library_HUDController
